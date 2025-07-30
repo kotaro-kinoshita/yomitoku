6e30168915e62248def50142796a1d2bc75aef73
kotaro-kinoshita <kotaro.masin.218@gmail.com>
2025-07-30 12:24:15 +0900

    fix

--- demo/simple_document_analysis.py
+++ demo/simple_document_analysis.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     for i, img in enumerate(imgs):
         results, ocr_vis, layout_vis = analyzer(img)
         # HTML形式で解析結果をエクスポート
-        # results.to_html(f"output_{i}.html", img=img)
-        results.to_json("output.json", img=img, encoding="shift-jis")
+        results.to_html(f"output_{i}.html", img=img)
         # 可視化画像を保存
         cv2.imwrite(f"output_ocr_{i}.jpg", ocr_vis)
         cv2.imwrite(f"output_layout_{i}.jpg", layout_vis)
